10ebc1980f890f20441deccfab6d16c7a992414d
mat1210 <matheuz1210@gmail.com>
2024-09-26 02:42:27 -0300

    increase default dbus timeout to 60s, add argument to change it

--- udisksctlex/__main__.py
+++ udisksctlex/__main__.py
@@ -6,9 +6,16 @@ from .dbus import *
 
 
 def main():
-    sdbus.set_default_bus(sdbus.sd_bus_open_system())
-
     parser = argparse.ArgumentParser("udisksctlex")
+    parser.add_argument(
+        "-t",
+        "--dbus-timeout",
+        type=int,
+        default=60,
+        metavar="SECS",
+        help="set timeout for dbus calls, in seconds",
+    )
+
     subparsers = parser.add_subparsers(title="commands", dest="command")
 
     loop_setup = subparsers.add_parser("loop-mount")
@@ -20,6 +27,9 @@ def main():
 
     args = parser.parse_args()
 
+    bus = sdbus.sd_bus_open_system()
+    bus.method_call_timeout_usec = args.dbus_timeout * 1000000
+    sdbus.set_default_bus(bus)
     match args.command:
         case "loop-mount":
             from .loop_open import loop_open
